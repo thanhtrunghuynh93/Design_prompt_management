25bc148fddceb4103240b76f095b3f82d91e59d1
vuviethuung1998 <vuviethung.98.hust@gmail.com>
2025-10-17 22:14:57 +0700

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from dotenv import load_dotenv
 from tools.metadata_extractor import describe_image_with_langchain, tagging_image_with_langchain
+import time
 
 load_dotenv()
 
@@ -14,7 +15,7 @@ df["description"] = ""
 df["tags"] = ""
 
 max_num_try = 3
-num_items = 10
+num_items = len(df)
 
 for i in range(num_items):
     print(i)
@@ -33,7 +34,14 @@ for i in range(num_items):
             break 
         except Exception as e:
             print(f"Attempt {attempt + 1} failed for item {i}: {e}")
+            time.sleep(60)  # Wait before retrying
             if attempt == max_num_try - 1:
                 print(f"Skipping item {i} after {max_num_try} tries.")
+            
+
+    # Save the updated dataframe to a new CSV file
+    if i % 10 == 0:  # Save every 10 items
+        df.to_csv("data_tagged.csv")
+        time.sleep(60)  # To avoid hitting rate limits
 
 df.to_csv("data_tagged.csv")
